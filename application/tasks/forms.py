463538ad5d43bd49873741d6f164fb0c265c2b7a
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-01-31 17:05:44 +0200

    Update new task settings Add login/logout

---
+++ application/tasks/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, BooleanField, validators
+
+class TaskForm(FlaskForm):
+	name = StringField("Task name", [validators.Length(min=2)])
+	urgency = IntegerField("Urgency", [validators.optional(), validators.NumberRange(min=1, max=3, message="Number must be between 1 and 3. (Leave empty if not urgent)")], render_kw={"placeholder": "Optional: 1-3"})
+	done = BooleanField("Done")
+
+	class Meta:
+		csrf = False
+
