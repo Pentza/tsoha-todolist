fe7f6fef92dcdff3cb884925e4f8f37be399df30
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-01-31 18:25:37 +0200

    Connect account to task

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -6,6 +6,10 @@ class Task(db.Model):
 	name = db.Column(db.String(144), nullable=False)
 	urgency = db.Column(db.Integer)
 	done = db.Column(db.Boolean, nullable=False)
+	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+	account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
 	def __init__(self, name):
 		self.name = name
--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,6 +1,6 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 from application.tasks.models import Task
 from application.tasks.forms import TaskForm
 
@@ -37,6 +37,7 @@ def tasks_create():
 	t = Task(form.name.data)
 	t.urgency = form.urgency.data
 	t.done = form.done.data
+	t.account_id = current_user.id
 
 	db.session().add(t)
 	db.session().commit()
