fda0dd8aed1591b4b9c2d6ff16770bf1f76f9498
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-02-06 13:19:06 +0200

    Add abstract class to database

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -1,13 +1,11 @@
 from application import db
 
-class Task(db.Model):
+class Task(Base):
 	id = db.Column(db.Integer, primary_key=True)
 
 	name = db.Column(db.String(144), nullable=False)
 	urgency = db.Column(db.Integer)
 	done = db.Column(db.Boolean, nullable=False)
-	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
 	account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
