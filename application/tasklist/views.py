e539c00fd237f9cdafda31c91dcfe18c51f7bbb6
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-02-15 21:42:56 +0200

    Update/bugfix

--- application/tasklist/views.py
+++ application/tasklist/views.py
@@ -3,7 +3,20 @@ from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 
 from application.tasklist.models import Tasklist
+from application.tasks.models import Task
 from application.tasklist.forms import TaskListForm
+
+
+@app.route("/tasklists", methods=["GET"])
+@login_required
+def tasklists_index():
+	return render_template("tasklist/list.html", tasklists = Tasklist.query.filter(Tasklist.account_id==current_user.id).all())
+
+@app.route("/tasklist/<list_id>", methods=["GET"])
+@login_required
+def show_tasklist(list_id):
+
+    return render_template("tasklist/list.html", tasks = Task.query.filter(Task.tasklist_id==list_id).all())
 
 @app.route("/tasklist/new")
 @login_required
@@ -13,8 +26,7 @@ def tasklist_form():
 @app.route("/tasklist/", methods=["POST"])
 @login_required
 def tasklist_create():
-    form = TaskListForm(request.form)
-
+    
     if not form.validate():
         return render_template("tasklist/new.html", form = form)
 
--- application/views.py
+++ application/views.py
@@ -1,8 +1,12 @@
 from flask import render_template
 from application import app
 from application.auth.models import User
+from application.tasklist.models import Tasklist
+from flask_login import login_required, current_user
 
 @app.route("/")
 def index():
-	return render_template("index.html", needs_tasks=User.find_users_with_no_tasks())
-
+	if current_user.is_authenticated:
+		return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), tasklists = Tasklist.query.filter(Tasklist.account_id==current_user.id).all())
+	else:
+		return render_template("index.html", needs_tasks=User.find_users_with_no_tasks(), tasklists =[("---")])
