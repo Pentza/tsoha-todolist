1136de9eb9f28a666d8e01247e138c31717e6a39
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-02-15 23:32:49 +0200

    Fix issues with heroku

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -9,11 +9,11 @@ class TaskForm(FlaskForm):
 	urgency = IntegerField("Urgency", [validators.optional(), validators.NumberRange(min=1, max=3, message="Number must be between 1 and 3. (Leave empty if not urgent)")], render_kw={"placeholder": "Optional: 1-3"})
 	done = BooleanField("Done")
 
-	tasklists_in_database = Tasklist.query.all()
+	#tasklists_in_database = Tasklist.query.all()
 
-	user_tasklists = [(Tasklist.id, Tasklist.name) for Tasklist in tasklists_in_database]
+	#user_tasklists = [(Tasklist.id, Tasklist.name) for Tasklist in tasklists_in_database]
 
-	tasklist = SelectField("TaskList", coerce=int, choices = user_tasklists)
+	#tasklist = SelectField("TaskList", coerce=int, choices = user_tasklists)
 
 	class Meta:
 		csrf = False
