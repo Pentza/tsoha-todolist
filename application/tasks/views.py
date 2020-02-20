cd095b674ebb2cd8996bc34ecdeceddd65f983c2
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-02-20 17:55:44 +0200

    Add edit_task function

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -2,7 +2,7 @@ from application import app, db
 from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 from application.tasks.models import Task
-from application.tasks.forms import TaskForm
+from application.tasks.forms import TaskForm, EditTaskForm
 
 @app.route("/tasks", methods=["GET"])
 @login_required
@@ -62,5 +62,18 @@ def tasks_create():
 @app.route("/task/edit/<task_id>", methods=["GET", "POST"])
 @login_required
 def task_edit(task_id):
-	return render_template("tasks/edit.html", task=Task.query.get(task_id))
+	t = Task.query.get(task_id)
+	if request.method == "GET":
+		return render_template("tasks/edit.html", task=t, form = EditTaskForm(new_name=t.name, new_urgency=t.urgency))
 
+	form = EditTaskForm(request.form)
+
+	if not form.validate():
+		return render_template("tasks/edit.html", task=t, form = form)
+
+	t.name = form.new_name.data
+	t.urgency = form.new_urgency.data
+
+	db.session().commit()
+
+	return redirect(url_for("show_tasklist", list_id = t.tasklist_id))
