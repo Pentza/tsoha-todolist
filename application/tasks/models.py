e097088402a99df007365b2b2b590fdf8730e612
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-02-21 17:47:14 +0200

    Add text import

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -1,6 +1,9 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
+
 class Task(Base):
 	id = db.Column(db.Integer, primary_key=True)
 
@@ -15,3 +18,19 @@ class Task(Base):
 	def __init__(self, name):
 		self.name = name
 		self.done = False
+
+
+	@staticmethod
+	def total_urgent_tasks(accountID):
+		stmt = text("SELECT Task.name, Tasklist.name FROM Account"
+			" LEFT JOIN Tasklist ON Tasklist.account_id = Account.id"
+			" LEFT JOIN Task ON Task.tasklist_id = Tasklist.id"
+			" WHERE Account.id = :accountID AND Task.urgency=3").params(accountID=accountID)
+
+		res = db.engine.execute(stmt)
+
+		response = []
+		for row in res:
+			response.append({"name":row[0], "listname":row[1]})
+
+		return response
