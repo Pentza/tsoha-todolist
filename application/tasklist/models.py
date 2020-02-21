1e2a9214db897997befa7ca4dc455ca12099c283
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2020-02-21 17:47:04 +0200

    Add text import

--- application/tasklist/models.py
+++ application/tasklist/models.py
@@ -1,6 +1,9 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
+
 class Tasklist(Base):
     name = db.Column(db.String(144), nullable = False)
 
@@ -10,19 +13,3 @@ class Tasklist(Base):
 
     def __init__(self, name):
         self.name = name
-
-#    @staticmethod
-#    def find_user_tasklist():
-#        stmt = text("SELECT Task.name FROM Task"
-#           " JOIN TaskList ON Task.account_id = Account.id"
-#            " WHERE (Task.done IS null OR Task.done = :done)"
-#            " GROUP BY Account.id"
-#            " HAVING COUNT(Task.id) = 0").params(done=done)
-#
-#        res = db.engine.execute(stmt)
-#
-#        response = []
-#        for row in res:
-#            response.append({"id":row[0], "name":row[1]})
-#
-#        return response
